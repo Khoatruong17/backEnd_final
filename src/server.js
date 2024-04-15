f3a907fc2a683902a44025085176f325b58d8e0f
Khoatruong17 <109325940+Khoatruong17@users.noreply.github.com>
2024-04-15 10:01:45 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ apiRouter(app);
 
 // CORS setup
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://vue-project-tu-wv3h.vercel.app/login");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
