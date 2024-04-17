27203058e41768219ebb3d353865b51f2d229a35
KhoaTruong17 <truongndkgch190486@email.com>
2024-04-17 22:55:53 +0700

    ok

--- src/config/database.js
+++ src/config/database.js
@@ -17,7 +17,6 @@ const dbState = [
 ];
 
 const connection = async () => {
-  console.log(process.env.DB_MONGOOSE);
   await mongoose.connect(process.env.DB_MONGOOSE);
   const state = Number(mongoose.connection.readyState);
   console.log(dbState.find((f) => f.value == state).label, "to db");
