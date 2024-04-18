491554610793ccaa1ef302c9ae8601fb22e19bde
KhoaTruong17 <truongndkgch190486@email.com>
2024-04-18 11:46:44 +0700

    ok

--- src/server.js
+++ src/server.js
@@ -51,8 +51,6 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
