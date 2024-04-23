72cc5aeba4e3c4dfd838d1d1ea1b1fdf76523019
Khoatruong17 <109325940+Khoatruong17@users.noreply.github.com>
2024-04-23 10:09:02 +0700

    ok

--- src/controllers/contribution.Controller.js
+++ src/controllers/contribution.Controller.js
@@ -204,12 +204,6 @@ const getAllContribution = async (req, res) => {
 const showcontributionbyFaculty = async (req, res) => {
   try {
     const headers = req.headers;
-    console.log(">>> Headers:", headers);
-
-    for (const header in headers) {
-      console.log(header + ": " + headers[header]);
-    }
-
     const authorizationHeader = headers.authorization;
     console.log("Authorization Header:", authorizationHeader);
     if (!authorizationHeader || authorizationHeader.length === 0) {
